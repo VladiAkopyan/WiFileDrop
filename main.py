f8790de80edf0b32f1d072b3c165fb9937c2e588
Gangster Duck <vakopan686@gmail.com>
2026-06-21 20:08:06 +0300

    Небольшие изменения

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from user_interface.program import *
+from user_interface.registration import *
+import json
+
+def user_name_find():
+    with open('./datas/user.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    return data['USER-NAME']
 
 if __name__ == '__main__':
-    app = MyApp()
-    app.mainloop()
+    if user_name_find() == '':
+        app = RegistrationApp()
+        app.mainloop()
+    else:
+        app = MyApp()
+        app.mainloop()
